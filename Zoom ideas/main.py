f36ec2eef06ef3e12597d73ce790c63733292a41
gonzoden <deniz.nazarova.99@gmail.com>
2021-07-26 00:26:23 +0600

    try branching

--- Zoom ideas/main.py
+++ Zoom ideas/main.py
@@ -1,10 +1,28 @@
 import random
 
-students = ['student0', 'student1', 'student2', 'student3', 'student3']
+f = open("students_list.txt", "a")
 
-def choose_student(students):
+def action_list(filename):
+	f = open("students_list.txt", "r")
+
+	for line in f.readlines():
+		if line != "end":
+			print(line)
+
+action_list(f)
+
+"""def choose_student(filename):
+	f = open(filename, "r")
+	quit_f = "not"
+	students = []
+
+	while quit_f != "end":
+		f = open(filename, "r")
+		quit_f = filename.splitlines()
+		students.append(quit_f)
+
 	ind = random.randrange(len(students))
 	chosen_one = students[ind]
 	print(chosen_one)
 
-choose_student(students)
+choose_student(f)"""
