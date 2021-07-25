b514490e7188374c2455fc17379914f9048f131c
gonzoden <deniz.nazarova.99@gmail.com>
2021-07-26 00:28:07 +0600

    keep experimenting with branching

--- Zoom ideas/main.py
+++ Zoom ideas/main.py
@@ -5,24 +5,18 @@ f = open("students_list.txt", "a")
 def action_list(filename):
 	f = open("students_list.txt", "r")
 
+	chosen_one
+	inds = 0
+
 	for line in f.readlines():
 		if line != "end":
-			print(line)
-
-action_list(f)
-
-"""def choose_student(filename):
-	f = open(filename, "r")
-	quit_f = "not"
-	students = []
-
-	while quit_f != "end":
-		f = open(filename, "r")
-		quit_f = filename.splitlines()
-		students.append(quit_f)
-
-	ind = random.randrange(len(students))
-	chosen_one = students[ind]
+			inds += 1 
+	
+	ind = random.randrange(inds)
+	
+	#TODO: add getting a certain line by inex
+	
+	#TODO: read on python memory allocation
 	print(chosen_one)
 
-choose_student(f)"""
+action_list(f)
