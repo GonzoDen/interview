e83128f8c148cae6faf8665626b23e0432b41543
gonzoden <deniz.nazarova.99@gmail.com>
2021-07-30 01:10:48 +0600

    update a problem

--- sort_indices.py
+++ sort_indices.py
@@ -1,6 +1,11 @@
+#show the indeces of the sorted array in a rising order
+## print the sorted array
+
 import numpy as np
 
 arr = np.array([1, 1, 0, 5, 2, 9, 7])
 
 arr_upd = np.argsort(arr)
-print ("indices in a rising order: ", arr_upd)
+print("indices in a rising order: ", arr_upd)
+
+print("sorted array: ", arr[arr_upd])
